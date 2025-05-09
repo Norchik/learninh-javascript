92859da495ae635348ac21b0e78c28dea6887a17
Norchik <111184028+Norchik@users.noreply.github.com>
2025-05-09 09:44:37 +0200

    added assignment for 6th and 7th may

---
+++ script.js
@@ -0,0 +1,62 @@
+// Assignment 6th of May 2025
+var age = prompt("enter your age")
+age > 18 ? console.log ('true')
+:console.log ('false')
+
+// let userinput = prompt("What is the “official” name of JavaScript?")
+// if (userinput =='ECMAScript')    {console.log("right!")    }
+// else {console.log('Didn’t know? It is ECMAScript!')}
+
+// let userinput = prompt('enter a number')
+// if (userinput > 0){console.log(alert(1))}
+// else if (userinput < 0){console.log(alert(-1))}
+// else if (userinput == 0){console.log(alert(0))}
+
+
+// 	let a = prompt('Write a random number',);
+// switch (a){
+//     case "0": console.log (alert(0))
+//      break;
+//     case "1":console.log (alert(1)) 
+//     break;
+//     case "2":console.log (alert(2)) 
+//     break;
+//     case "3":console.log (alert(3)) 
+//     break;
+//     default: console.log(alert('Your number is greater than 3, this is your number: ' + a))
+// }
+
+// Assignment 7th May 2025
+// let arr = [];
+
+// for (let i = 0; i <= 100; ++i) {
+//     if (i % 2 === 1) {
+//         console.log(i)
+//         arr.push(i);
+//     }
+// }
+
+// let marr = [];
+
+// for (let i = 0; i <= 100; ++i) {
+//     if (i % 2 === 0) {
+//         console.log(i)
+//         marr.push(i);
+//     }
+// }
+
+
+// function fizzBuzz() {
+//     for (let n = 1; n <= 100; n++) {
+//         if (n % 3 === 0 && n % 5 === 0) {
+//             console.log("FizzBuzz");
+//         } else if (n % 3 === 0) {
+//             console.log("Fizz");
+//         } else if (n % 5 === 0) {
+//             console.log("Buzz");
+//         } else {
+//             console.log(n);
+//         }
+//     }
+// }
+// fizzBuzz();
